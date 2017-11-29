58686aef37c5306d48c7efc6bd6cb9dacc277f3a
Merrick Sapsford <merrick@sapsford.tech>
2017-11-29 14:20:30 +0100

    Add additional routing

--- Sources/App/Routes/Routes.swift
+++ Sources/App/Routes/Routes.swift
@@ -2,16 +2,6 @@ import Vapor
 
 extension Droplet {
     func setupRoutes() throws {
-        get("hello") { req in
-            var json = JSON()
-            try json.set("hello", "world")
-            return json
-        }
-
-        get("plaintext") { req in
-            return "Hello, world!"
-        }
-
         // response to requests to /info domain
         // with a description of the request
         get("info") { req in
@@ -20,9 +10,13 @@ extension Droplet {
 
         get("description") { req in return req.description }
         
+        try resource("posts", PostController.self)
+        
+        // Workouts
         let workoutController = WorkoutController()
         resource("workouts", workoutController)
-        
-        try resource("posts", PostController.self)
+        get("workouts", "*") { request in
+            return try workoutController.loadAll(request)
+        }
     }
 }
