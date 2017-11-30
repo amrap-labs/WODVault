b76303c911ba7abcc1be431795b6fcbe0d01f58f
Merrick Sapsford <merrick@sapsford.tech>
2017-11-30 21:06:20 +0100

    Fix incorrect packages

--- Package.swift
+++ Package.swift
@@ -13,10 +13,10 @@ let package = Package(
         .package(url: "https://github.com/vapor/fluent-provider.git", .upToNextMajor(from: "1.2.0")),
         .package(url: "https://github.com/BrettRToomey/Jobs.git", .upToNextMajor(from: "1.1.2")),
         .package(url: "https://github.com/nmdias/FeedKit.git", .upToNextMajor(from: "6.0.0")),
-        .package(url: "https://github.com/vapor-community/postgresql-driver.git", .upToNextMajor(from: "2.0.0"))
+        .package(url: "https://github.com/vapor-community/postgresql-provider.git", .upToNextMajor(from: "2.0.0"))
     ],
     targets: [
-        .target(name: "App", dependencies: ["Vapor", "FluentProvider", "Jobs", "FeedKit", "PostgreSQLDriver"],
+        .target(name: "App", dependencies: ["Vapor", "FluentProvider", "Jobs", "FeedKit", "PostgreSQLProvider"],
                 exclude: [
                     "Config",
                     "Public",
